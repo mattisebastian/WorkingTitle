d8307f49b30e5ea1fe9f9072d7c6a42870c92042
Matti <matti.leydecker@gmx.de>
2015-01-21 15:52:40 +0100

    added zoom controls added mylocation button

--- app/src/main/java/de/admuc/gruppe12/workingtitle/MapsActivity.java
+++ app/src/main/java/de/admuc/gruppe12/workingtitle/MapsActivity.java
@@ -1,10 +1,19 @@
 package de.admuc.gruppe12.workingtitle;
 
 import android.content.Context;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -12,17 +21,80 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+/**
+ * This class creates and wraps the Google Maps View.
+ * It also zooms to the last known location and generates periodic updates to the users location
+ */
 
+public class MapsActivity extends FragmentActivity implements
+        ConnectionCallbacks, OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
+
+    // Dummy data
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
+    /**
+     * Stores parameters for requests to the FusedLocationProviderApi.
+     */
+    protected LocationRequest mLocationRequest;
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    /**
+     * Entry point to the GoogleMaps API
+     */
+    private GoogleApiClient mGoogleApiClient;
+    /**
+     * The last location as provided by the Maps API
+     */
+    private Location mLastLocation;
+
+    /**
+     * Set by the user if he wants to receive location updates
+     */
+    private boolean mRequestingLocationUpdates = true;
+
+
+    /**
+     * The Google API needs this one
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        createLocationRequest();
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        moveCameraToLastLocation();
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        buildGoogleApiClient();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
 
     @Override
@@ -30,7 +102,6 @@ public class MapsActivity extends FragmentActivity {
         super.onResume();
         setUpMapIfNeeded();
     }
-
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
@@ -65,6 +136,11 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
+
+        // basic ui settings
+        mMap.setMyLocationEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
         Marker hamburg = mMap.addMarker(new MarkerOptions().position(HAMBURG)
                 .title("Hamburg"));
@@ -92,4 +168,39 @@ public class MapsActivity extends FragmentActivity {
 
     }
 
+    private void moveCameraToLastLocation() {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            CameraUpdate center =
+                    CameraUpdateFactory.newLatLng(new LatLng(mLastLocation.getLatitude(),
+                            mLastLocation.getLongitude()));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
+
+            mMap.moveCamera(center);
+            mMap.animateCamera(zoom);
+        }
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
 }
